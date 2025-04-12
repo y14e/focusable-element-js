7fcb3f08aeb0e71d378076a2672850488c6e8fbd
Yusuke Kamiyamane <y@y14e.com>
2025-04-12 10:01:42 +0900

    Initial commit

---
+++ focusable-element.js
@@ -0,0 +1,10 @@
+const FOCUSABLE_SELECTOR = 'a[href], area[href], button:not([disabled]), embed, iframe, input:not([disabled]):not([type="hidden"]), object, select:not([disabled]), textarea:not([disabled]), [contenteditable]:not([contenteditable="false"]), [tabindex]:not([tabindex="-1"])';
+
+export function hasFocusableElement(element) {
+  return !!element.querySelector(FOCUSABLE_SELECTOR);
+}
+
+export function getFocusableElements(element) {
+  if (!hasFocusableElement(element)) return [];
+  return [...element.querySelectorAll(FOCUSABLE_SELECTOR)];
+}
