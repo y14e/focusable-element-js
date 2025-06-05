6ea99dd42492a8ee16ee7705365e58af3e1a25f2
Yusuke Kamiyamane <y@y14e.com>
2025-06-05 23:34:06 +0900

    Update focusable-element.js

--- focusable-element.js
+++ focusable-element.js
@@ -1,4 +1,4 @@
-const FOCUSABLE_SELECTOR = ':is(a[href], area[href], button, embed, iframe, input:not([type="hidden"]), object, select, textarea, [contenteditable]:not([contenteditable="false"]), [tabindex]):not([disabled], [tabindex="-1"])';
+const FOCUSABLE_SELECTOR = ':is(a[href], area[href], button, embed, iframe, input:not([type="hidden"]), object, select, textarea, [contenteditable]:not([contenteditable="false"]), [tabindex]):not([disabled], [hidden], [tabindex="-1"])';
 
 export function hasFocusableElement(element) {
   return !!getFocusableElements(element).length;
