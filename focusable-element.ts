2cc049f2026b3c8bbdf40fb5bb113239476f8472
Yusuke Kamiyamane <y@y14e.com>
2025-10-21 21:03:12 +0900

    Update focusable-element.ts

--- focusable-element.ts
+++ focusable-element.ts
@@ -1,8 +1,8 @@
-type FocusableElementOptions = {
+interface FocusableElementOptions {
   current?: HTMLElement | null;
   offset: number;
   wrap?: boolean;
-};
+}
 
 const FOCUSABLE_SELECTOR = ':is(a[href], area[href], button, embed, iframe, input:not([type="hidden"]), object, select, details > summary:first-of-type, textarea, [contenteditable]:not([contenteditable="false"]), [controls], [tabindex]):not([disabled], [hidden], [tabindex="-1"])';
 
