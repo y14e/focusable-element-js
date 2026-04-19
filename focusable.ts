ff9d163d509be36f4e597c9a1a481db0c1bbc3d6
Yusuke Kamiyamane <y@y14e.com>
2026-04-19 18:34:31 +0900

    Update focusable.ts

--- focusable.ts
+++ focusable.ts
@@ -1,5 +1,4 @@
-const FOCUSABLE_SELECTOR =
-  ':is(a[href], area[href], button, embed, iframe, input:not([type="hidden"]), object, select, details > summary:first-of-type, textarea, [contenteditable]:not([contenteditable="false"]), [controls], [tabindex]):not([aria-disabled="true"], :disabled, [hidden], [inert], [tabindex="-1"])';
+const FOCUSABLE_SELECTOR = `:is(a[href], area[href], button, embed, iframe, input:not([type="hidden"]), object, select, details > summary:first-of-type, textarea, [contenteditable]:not([contenteditable="false"]), [controls], [tabindex]):not([aria-disabled="true"], :disabled, [hidden], [inert], [tabindex="-1"])`;
 
 export interface FocusableOptions {
   readonly active?: HTMLElement | null;
